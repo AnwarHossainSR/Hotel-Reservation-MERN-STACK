928cbe5ce3949ba5988b2a1709427d6790af79b8
AnwarHossainSR <mahedianwar@gmail.com>
2021-07-28 01:44:19 +0600

    Added Mongo DB

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require("express");
 
 const app = express();
+
+const dbConfig = require('./db.js');
+
 const port = process.env.PORT || 6000;
 app.listen(port, () => console.log(`Node server started`));
